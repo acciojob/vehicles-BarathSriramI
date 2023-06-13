7d64e0b7d777cef014a92b1823e96d3763b1f0c1
barath sriram <barathsriramiyyappan@gmail.com>
2023-06-13 19:25:13 +0530

    completed

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -11,14 +11,26 @@ public class Car extends Vehicle {
 
     public Car(String name, int wheels, int doors, int gears, boolean isManual, String type, int seats) {
         //Hint: Car extends Vehicle
+        super(name);
+        this.currentGear=1;
+        this.wheels=wheels;
+        this.doors=doors;
+        this.gears=gears;
+        this.isManual=isManual;
+        this.type=type;
+        this.seats=seats;
+
     }
 
     public void changeGear(int newGear){
-
+            this.currentGear = newGear;
         System.out.println("changeGear method called - The gear is changed to: " + currentGear);
     }
 
     public void changeSpeed(int newSpeed, int newDirection){
+       // move(newSpeed,newDirection);// no move in the car class so java will find it in parent class
+        super.move(newSpeed,newDirection);// correct way to call a method in parent class
+        //this.move(newSpeed,newDirection);// will call move() in the this class , can't find it then moves to parent
 
         System.out.println("changeSpeed method called - The speed is changed to: " + newSpeed + ", and the direction is changed to: " + newDirection + " degrees");
     }
